20dfd200fff1802cf5d70f4eb3d1b09076227eb0
spoilr <oana160692@yahoo.com>
2020-04-28 21:00:42 +0100

    flat data

---
+++ flat_data.py
@@ -0,0 +1,82 @@
+merged_flat = dict()
+
+merged_flat['model'] = ['supports(b1,b0)', 'supports(b2,b1)', 'supports(b3,b1)', 'attacks(b4,b1)', 'attacks(b6,b1)', 'attacks(b5,b4)', 'supports(b8,b6)', 'attacks(b7,b3)', 'supports(b11,b5)', 'attacks(b10,b8)', 'attacks(b9,b7)']
+merged_flat['meng1'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['meng2'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['meng3'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['meng4'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['meng5'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b6,b0)', 'attacks(b5,b4)', 'supports(b8,b6)', 'attacks(b7,b3)', 'supports(b11,b5)', 'attacks(b10,b6)', 'attacks(b9,b7)']
+merged_flat['meng6'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['meng7'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['meng8'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['meng9'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'supports(b5,b4)', 'attacks(b6,b0)', 'supports(b7,b3)', 'supports(b10,b6)', 'attacks(b11,b5)']
+merged_flat['meng10'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['meng11'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'supports(b8,b6)', 'supports(b9,b3)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['meng12'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['mac13'] = ['supports(b1,b0)','supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'supports(b7,b6)', 'attacks(b8,b6)', 'attacks(b9,b6)', 'attacks(b9,b7)', 'supports(b9,b8)', 'supports(b10,b0)', 'supports(b11,b0)', 'supports(b11,b5)', 'attacks(b11,b4)']
+merged_flat['mac14'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'supports(b8,b6)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['mac15'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'attacks(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['msc16'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'attacks(b7,b0)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b6)', 'supports(b11,b5)']
+merged_flat['mscai17'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b0)', 'attacks(b8,b7)', 'supports(b10,b0)', 'attacks(b11,b4)']
+merged_flat['mscai18'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'attacks(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+merged_flat['mscai19'] = ['supports(b1,b0)', 'supports(b2,b0)', 'supports(b3,b0)', 'attacks(b4,b0)', 'attacks(b5,b4)', 'attacks(b6,b0)', 'attacks(b7,b3)', 'supports(b8,b6)', 'attacks(b9,b7)', 'attacks(b10,b8)', 'supports(b11,b5)']
+
+merged_flat_script = ['Mary should rent the flat.', 							#R0
+'The flat is near Mary\'s work',												#A1
+'The flat is located in a well illuminated and safe area.',						#B2
+'The flat is in a quiet area, because most of the neighbours are retired.',		#C3
+'The flat is tiny.',															#D4
+'Despite the size, spaces are well distributed within the flat.',				#E5
+'The flat shows signs of humidity problems.',									#F6
+'There are rumours that a nightclub will open in the area.',					#G7
+'Humidity problems take time to resolve.',										#H8
+'Local legislation forbids the opening of a nightclub in the area.',			#I9
+'The building\'s insurance covers fixing any problems promptly.',				#L10
+'A renowned architect designed the building to optimise space distribution.'	#M11
+]
+
+
+
+
+unmerged_flat = dict()
+
+unmerged_flat['model'] = ['supports(c1,c0)', 'supports(c2,c1)', 'supports(c3,c1)', 'attacks(c4,c1)', 'attacks(c6,c1)', 'attacks(c5,c4)', 'supports(c8,c6)', 'attacks(c7,c3)', 'supports(c11,c5)', 'attacks(c10,c8)', 'attacks(c9,c7)']
+unmerged_flat['meng1'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c14)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['meng2'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['meng3'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['meng4'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['meng5'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c6,c0)', 'attacks(c5,c4)', 'supports(c8,c6)', 'attacks(c7,c14)', 'supports(c11,c5)', 'attacks(c10,c6)', 'attacks(c9,c7)']
+unmerged_flat['meng6'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['meng7'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['meng8'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['meng9'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'supports(c5,c4)', 'attacks(c6,c0)', 'supports(c7,c3)', 'supports(c10,c6)', 'attacks(c11,c5)']
+unmerged_flat['meng10'] = ['supports(c12,c0)', 'supports(c13,c0)', 'supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c14)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['meng11'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'supports(c8,c6)', 'supports(c9,c3)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['meng12'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['mac13'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)','supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'supports(c7,c6)', 'attacks(c8,c6)', 'attacks(c9,c6)', 'attacks(c9,c7)', 'supports(c9,c8)', 'supports(c10,c0)', 'supports(c11,c0)', 'supports(c11,c5)', 'attacks(c11,c4)']
+unmerged_flat['mac14'] = ['supports(c12,c0)', 'supports(c13,c0)', 'supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'supports(c8,c6)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['mac15'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c14)', 'attacks(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['msc16'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c14)', 'attacks(c7,c0)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c6)', 'supports(c11,c5)']
+unmerged_flat['mscai17'] = ['supports(c14,c0)', 'supports(c15,c14)', 'supports(c1,c0)', 'supports(c2,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c0)', 'attacks(c8,c7)', 'supports(c10,c0)', 'attacks(c11,c4)']
+unmerged_flat['mscai18'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c3)', 'attacks(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+unmerged_flat['mscai19'] = ['supports(c1,c0)', 'supports(c2,c0)', 'supports(c3,c0)', 'attacks(c4,c0)', 'attacks(c5,c4)', 'attacks(c6,c0)', 'attacks(c7,c3)', 'supports(c8,c6)', 'attacks(c9,c7)', 'attacks(c10,c8)', 'supports(c11,c5)']
+
+
+unmerged_flat_script = ['Mary should rent the flat.',							#R0
+'The flat is near Mary\'s work.',												#A1
+'The flat is located in a well illuminated and safe area.',						#B2
+'The flat is in a quiet area, because most of the neighbours are retired.',		#C3
+'The flat is tiny.',															#D4
+'Despite the size, spaces are well distributed within the flat.',				#E5
+'The flat shows signs of humidity problems.',									#F6
+'There are rumours that a nightclub will open in the area.',					#G7
+'Humidity problems take time to resolve.',										#H8
+'Local legislation forbids the opening of a nightclub in the area.',			#I9
+'The building\'s insurance covers fixing any problems promptly.',				#L10
+'A renowned architect designed the building to optimise space distribution.',	#M11
+'The flat is located in a well illuminated area.',								#B21	12
+'The flat is located in a safe area.',											#B22	13
+'The flat is in a quiet area.',													#C31 	14
+'Most of the neighbours are retired.',											#C32 	15
+]
+
