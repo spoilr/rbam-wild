da7b2704ecbc812711f8deb8a7cce0b6ed6b0946
spoilr <oana160692@yahoo.com>
2020-04-28 21:00:04 +0100

    data from angry men script

---
+++ angry_men_data.py
@@ -0,0 +1,69 @@
+angry_men = dict()
+
+angry_men['1'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a5,a0)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'attacks(a17,a16)', 'supports(a2,a0)', 'supports(a5,a4)', 'supports(a6,a4)', 'supports(a11,a10)']
+
+angry_men['2'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a5,a4)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a17,a16)', 'supports(a6,a0)', 'supports(a10,a1)', 'supports(a11,a10)', 'supports(a12,a7)', 'supports(a12,a5)', 'supports(a13,a1)', 'supports(a13,a5)', 'supports(a16,a1)']
+
+angry_men['3'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a6,a1)', 'attacks(a7,a6)', 'attacks(a9,a8)', 'attacks(a12,a0)', 'attacks(a13,a1)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a17,a16)', 'supports(a4,a0)', 'supports(a5,a1)', 'supports(a8,a1)', 'supports(a10,a1)', 'supports(a11,a1)', 'supports(a16,a1)']
+
+angry_men['4'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a5,a4)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a6,a0)', 'supports(a10,a1)', 'supports(a11,a10)', 'supports(a12,a7)', 'supports(a13,a1)', 'supports(a16,a1)', 'supports(a17,a16)']
+
+angry_men['5'] = ['attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a4,a1)', 'supports(a5,a1)', 'supports(a6,a4)', 'attacks(a7,a6)', 'supports(a8,a1)', 'attacks(a9,a8)', 'attacks(a12,a0)', 'supports(a13,a1)', 'attacks(a14,a13)', 'supports(a16,a1)', 'attacks(a17,a16)']
+
+angry_men['6'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a5,a4)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a9)', 'supports(a11,a10)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'supports(a14,a13)', 'supports(a15,a14)', 'attacks(a16,a0)', 'supports(a17,a16)']
+
+angry_men['7'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a4,a3)', 'attacks(a5,a0)', 'supports(a6,a4)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a16,a10)', 'attacks(a17,a16)']
+
+angry_men['8'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'supports(a4,a2)', 'attacks(a5,a4)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'supports(a10,a1)', 'supports(a11,a10)', 'supports(a12,a11)', 'supports(a13,a12)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a9)', 'supports(a17,a16)']
+
+angry_men['9'] = ['attacks(a1,a0)', 'attacks(a2,a1)', 'supports(a2,a0)', 'attacks(a3,a2)', 'attacks(a3,a0)', 'attacks(a5,a0)', 'attacks(a6,a5)', 'supports(a6,a4)', 'supports(a6,a0)', 'attacks(a7,a0)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a15,a13)', 'supports(a17,a0)', 'supports(a17,a16)']
+
+angry_men['10'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a4,a1)', 'attacks(a5,a0)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'attacks(a17,a16)', 'supports(a17,a0)']
+
+angry_men['11'] = ['attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a5,a4)', 'attacks(a6,a5)', 'supports(a6,a4)', 'attacks(a7,a6)', 'supports(a8,a3)', 'attacks(a8,a6)', 'attacks(a9,a8)', 'attacks(a10,a9)', 'supports(a11,a10)', 'supports(a12,a11)', 'supports(a13,a9)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a16,a15)', 'attacks(a17,a16)', 'attacks(a17,a11)', 'supports(a2,a0)', 'attacks(a1,a0)', 'attacks(a3,a0)', 'attacks(a5,a0)', 'attacks(a8,a0)', 'attacks(a10,a0)', 'attacks(a13,a0)', 'attacks(a16,a0)']
+
+angry_men['12'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a5,a0)', 'supports(a6,a0)', 'attacks(a7,a6)', 'supports(a8,a5)', 'attacks(a9,a8)', 'attacks(a10,a9)', 'supports(a11,a10)', 'supports(a12,a10)', 'supports(a13,a1)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a16,a10)', 'supports(a17,a16)']
+
+angry_men['13'] = ['attacks(a1,a0)', 'attacks(a3,a2)', 'attacks(a5,a0)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a11,a0)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a0)', 'attacks(a16,a0)', 'attacks(a17,a16)', 'supports(a2,a0)', 'supports(a3,a1)', 'supports(a4,a0)', 'supports(a5,a1)', 'supports(a6,a0)', 'supports(a7,a5)', 'supports(a9,a0)', 'supports(a11,a10)', 'supports(a12,a10)', 'supports(a14,a0)', 'supports(a15,a13)', 'supports(a17,a0)']
+
+angry_men['14'] = ['attacks(a1,a0)', 'attacks(a3,a2)', 'attacks(a5,a4)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'attacks(a17,a16)', 'supports(a2,a0)', 'supports(a4,a0)', 'supports(a11,a10)']
+
+angry_men['15'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'attacks(a4,a3)', 'attacks(a5,a4)', 'supports(a6,a4)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'supports(a17,a16)']
+
+angry_men['16'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a4,a0)', 'attacks(a5,a4)', 'supports(a6,a0)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'supports(a12,a10)', 'supports(a13,a10)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a16,a10)', 'supports(a17,a16)']
+
+angry_men['17'] = ['attacks(a0,a1)', 'attacks(a1,a0)', 'attacks(a2,a1)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a3,a1)', 'attacks(a5,a4)', 'supports(a5,a1)', 'attacks(a6,a5)', 'supports(a6,a4)', 'attacks(a7,a6)', 'supports(a7,a5)', 'supports(a7,a1)', 'supports(a8,a7)', 'supports(a8,a1)', 'attacks(a8,a6)', 'attacks(a9,a8)', 'attacks(a10,a9)', 'supports(a10,a0)', 'supports(a11,a10)', 'supports(a12,a11)', 'attacks(a12,a9)', 'supports(a13,a10)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'supports(a15,a13)', 'supports(a16,a0)', 'supports(a17,a0)', 'supports(a17,a16)']
+
+angry_men['18'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a4,a0)', 'attacks(a5,a0)', 'supports(a6,a4)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'attacks(a12,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'attacks(a17,a16)']
+
+angry_men['19'] = ['attacks(a3,a2)', 'attacks(a5,a0)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a17,a16)', 'supports(a2,a0)', 'supports(a3,a1)', 'supports(a6,a4)', 'supports(a9,a4)', 'supports(a11,a10)', 'supports(a12,a7)', 'supports(a13,a3)']
+
+angry_men['20'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a4,a2)', 'attacks(a5,a4)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'supports(a11,a10)', 'supports(a12,a10)', 'attacks(a13,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'supports(a17,a16)']
+
+angry_men['21'] = ['attacks(a1,a0)', 'supports(a2,a0)', 'attacks(a3,a2)', 'supports(a2,a4)', 'attacks(a6,a7)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a14,a13)', 'supports(a15,a13)', 'supports(a17,a16)']
+
+model = ['attacks(a1,a0)', 'attacks(a2,a1)', 'attacks(a3,a2)', 'attacks(a5,a0)', 'attacks(a6,a5)', 'attacks(a7,a6)', 'attacks(a8,a0)', 'attacks(a9,a8)', 'attacks(a10,a0)', 'attacks(a14,a13)', 'attacks(a15,a14)', 'attacks(a16,a0)', 'attacks(a17,a16)', 'supports(a4,a0)', 'supports(a11,a10)', 'supports(a12,a11)', 'supports(a13,a12)']
+
+
+
+angry_men_script = ['The boy is not guilty.',
+'I never saw a guiltier man in my life. You sat right in court and heard the same thing I did. The man\'s a dangerous killer.',
+'He\'s nineteen years old [he is too young to be guilty]',
+'That\'s old enough. He knifed his own father, four inches into the chest. An innocent little nineteen-year old kid. They proved it a dozen different ways.',
+'It\'s not so easy for me to raise my hand and send a boy off to die without talking about it first.',
+'I think the guy\'s guilty. You couldn\'t change my mind if you talked for a hundred years.',
+'This boy\'s been kicked around all his life. You know, living in a slum, his mother dead since he was nine. That\'s not a very good head start. He\'s a tough, angry kid. I think maybe we owe him a few words [before considering him guilty].',
+'We don\'t owe him a thing. He got a fair trial, didn\'t he? You\'re not going to tell us that we\'re supposed to believe him, knowing what he is. I\'ve lived among \'em all my life. You can\'t believe a word they say.',
+'I just think he\'s guilty. I thought it was obvious. I mean nobody proved otherwise.',
+'Nobody has to prove otherwise. The burden of proof is on the prosecution. The defendant doesn\'t have to open his mouth. That\'s in the Constitution. The Fifth Amendment.',
+# 'Okay, let\'s get to the facts. Number one, let\'s take the old man who lived on the second floor right underneath the room where the murder took place. At ten minutes after twelve on the night of the killing he heard loud noises in the upstairs apartment. He said it sounded like a fight. Then he heard the kid say to his father, \I\'m gonna kill you.!" A second later he heard a body falling, and he ran to the door of his apartment, looked out, and saw the kid running down the stairs and out of the house. Then he called the police. They found the father with a knife in his chest.',
+'Minutes after twelve the night of the killing he heard loud noises in the apartment. He said it sounded like a fight. He heard the kid say to his father, \I\'m gonna kill you.!" He heard a body falling, saw the kid running down the stairs out of the house.',
+'And the coroner fixed the time of death at around midnight.',
+'He claimed he was at the movies. That\'s a little ridiculous, isn\'t it? He couldn\'t even remember what pictures he saw.',
+'What about the woman across the street? If her testimony [to have seen the killing] don\'t prove [that the boy is guilty], then nothing does.',
+'[She saw the killing] through the windows of a passing elevated train [so her testimony is not reliable].',
+'And they proved in court that you can look through the windows of a passing train at night and see what\'s happening on the other side. They proved it.',
+'I started to be convinced, you know, with the testimony from those people across the hall. Didn\'t they say something about an argument between the father and the boy around seven o\'clock that night?',
+'Eight o\'clock. They heard the father hit the boy twice and then saw the boy walk angrily out of the house. [It does not prove that the boy killed him].'
+]
+
